8cb368827aeec38809fdca522b3b58199ba2c90b
Diego Navarrete <diego.navarrete2@mail.udp.cl>
2025-07-22 22:50:14 -0400

    MVP

---
+++ endpoints/players_from_a_team.py
@@ -0,0 +1,36 @@
+# Buscamos obtener una lista de todos los jugadores de un equipo en específico
+import psycopg2
+import os
+import json
+from db import get_connection
+
+#Necesitamos el id del equipo
+def get_players_from_team(team_id):
+    conn = get_connection()
+    cur = conn.cursor()
+
+    try:
+        cur.execute("SELECT * FROM jugadores WHERE id_equipo = %s;", (team_id,))
+        rows = cur.fetchall()
+        
+        # Procesamos los resultados a un formato JSON
+        players = []
+        for row in rows:
+            player = {
+                "id_jugador": row[0],
+                "nombre": row[1],
+                "apellido": row[2],
+                "nacionalidad": row[3],
+                "fecha_nacimiento": row[4].isoformat() if row[4] else None,
+                "posicion": row[5],
+                "id_equipo": row[6],
+            }
+            players.append(player)
+
+        return json.dumps(players, ensure_ascii=False)
+    
+    except Exception as e:
+        print(f"Error al obtener los jugadores del equipo {team_id}: {e}")
+        return []
+    
+print(get_players_from_team(1))  
---
+++ endpoints/teams.py
@@ -0,0 +1,34 @@
+#Buscamos obtener una lista en JSON de todos los equipos}
+import psycopg2
+import os
+import json
+from db import get_connection
+
+def get_teams():
+    conn = get_connection()
+    cur = conn.cursor()
+
+    try:
+        cur.execute("SELECT * FROM equipos;")
+        rows = cur.fetchall()
+        #Hasta aquí obtenemos los datos.
+
+        # Proceso para pasar los resultados a un formato JSON
+        teams = []
+        for row in rows:
+            team = {
+                "id": row[0],
+                "nombre": row[1],
+                "pais": row[2],
+                "entrenador": row[3],
+                "liga": row[4],
+            }
+            teams.append(team)
+
+        return json.dumps(teams, ensure_ascii=False)
+
+    except Exception as e:
+        print(f"Error al obtener los equipos: {e}")
+        return []
+    
+print(get_teams())
